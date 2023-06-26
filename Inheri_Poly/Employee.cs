28221d60987cb2ec485d8002106b0add8522dc17
SnehalKawade <snehkawade@gmail.com>
2023-06-26 15:14:16 +0530

    Add project files.

---
+++ Inheri_Poly/Employee.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheri_Poly
+{
+    public class Inheri_Poly 
+    {
+        protected string ename;
+        protected double basesal, gs, hra, ta, da, pf, total;
+        protected int empid;
+        private static int count;
+        public Inheri_Poly(string ename, double basesal)
+        {
+            count++;
+            this.empid = count;
+            this.ename = ename;
+            this.basesal = basesal;
+        }
+        public virtual void CalulateSalary()
+        {
+            hra = basesal * 0.40;
+            ta = basesal * 0.20;
+            da = basesal * 0.15;
+            pf = basesal * 0.12;
+
+            total = (hra + da + ta);
+
+            gs = (hra + da + ta) - pf;
+        }
+        public override string ToString()
+        {
+            return $"name is {ename}\n id is {empid} \nbasic salary is {basesal}\n" +
+                $"gross salary is {gs}";
+        }
+        
+    }
+    public class SalesManager : Inheri_Poly
+    {
+        private double comm;
+
+        public SalesManager(string ename, double basesal, double comm) : base(ename, basesal)
+        {
+            this.comm = comm;
+        }
+        public override void CalulateSalary()
+        {
+            hra = basesal * 0.40;
+            ta = basesal * 0.20;
+            da = basesal * 0.15;
+            pf = basesal * 0.12;
+
+            total = (hra + da + ta + comm);
+
+            gs = (total) - pf;
+        }
+        public override string ToString()
+        {
+            return $"\n\nname is {ename}\n id is {empid} \nbasic salary is {basesal}\ntotal salary {total}\n" +
+                $"gross salary is {gs}";
+        }
+    }
+
+    public class Ceo : Inheri_Poly
+    {
+        private double loss;
+
+        public Ceo(string ename, double basesal, double loss) : base(ename, basesal)
+        {
+            this.loss = loss;
+        }
+        public override void CalulateSalary()
+        {
+            hra = basesal * 0.40;
+            ta = basesal * 0.20;
+            da = basesal * 0.15;
+            pf = basesal * 0.12;
+
+            total = (hra + da + ta);
+
+            gs = (total) - pf - loss;
+        }
+        public override string ToString()
+        {
+            return $"\n\nname is {ename}\n id is {empid} \nbasic salary is {basesal}\ntotal salary {total}\n" +
+                $"gross salary is {gs}";
+        }
+    }
+}
---
+++ Inheri_Poly/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheri_Poly
+{
+    public  class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Inheritance and polymorphism
+            Inheri_Poly e1 = new Inheri_Poly("sagar", 20000);
+            e1.CalulateSalary();
+            // Console.WriteLine(e1.PrintD());
+            Console.WriteLine(e1.ToString());
+
+            SalesManager sm1 = new SalesManager("don", 20000, 6500);
+            sm1.CalulateSalary();
+            Console.WriteLine(sm1);
+
+            Ceo ceo = new Ceo("om", 30000, 5);
+            ceo.CalulateSalary();
+            Console.WriteLine(ceo);
+        }
+    }
+}
